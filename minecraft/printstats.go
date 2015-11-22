fb95dcbfbd0a72f5d942cd225cc494f3210a1574
Andrew Hurst <andrew@hurstdog.org>
2015-11-21 22:05:39 -0800

    Actually print out some basic statistics

--- minecraft/printstats.go
+++ minecraft/printstats.go
@@ -2,14 +2,22 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"loginstats"
+	"github.com/hurstdog/tools/minecraft/loginstats"
 	"os"
 )
 
 func main() {
 	for _, f := range os.Args[1:] {
-		loginstats.ReadLog(f)
+		err := loginstats.ReadLog(f)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading %s: %q", f, err)
+		}
+	}
+	stats := loginstats.GetAllStats()
+	for k, v := range stats {
+		fmt.Printf("Username: %s\n", k)
+		fmt.Printf("Login Count: %d\n", v.LoginCount)
+		fmt.Printf("Total Play Time: %d minutes\n", v.TotalPlayTime)
 	}
 }
