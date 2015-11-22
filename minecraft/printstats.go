b22f3853958d490e9a7ca48f4e14f932e7900266
Andrew Hurst <andrew@hurstdog.org>
2015-11-21 22:26:26 -0800

    Added support for reading compressed logs.

--- minecraft/printstats.go
+++ minecraft/printstats.go
@@ -2,14 +2,28 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
 	"github.com/hurstdog/tools/minecraft/loginstats"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, f := range os.Args[1:] {
-		err := loginstats.ReadLog(f)
+		fh, err := os.Open(f)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening %s: %q", f, err)
+		}
+		if strings.HasSuffix(f, ".gz") {
+			gz, err := gzip.NewReader(fh)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error reading %s: %q", f, err)
+			}
+			err = loginstats.ReadLog(gz)
+		} else {
+			err = loginstats.ReadLog(fh)
+		}
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error reading %s: %q", f, err)
 		}
